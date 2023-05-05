4e1f731cfc27d29e52d168c21d2abc5854167e4e
Gleb <glebbus89@gmail.com>
2023-05-05 09:57:19 +0300

    Добавлено домашнее задание

---
+++ Program.cs
@@ -0,0 +1,193 @@
+﻿// Task 1. Задайте двумерный массив размером m×n, заполненный случайными целыми числами.
+
+/*int[,] CreateRandom2dArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] array = new int[rows, columns];
+
+    for(int i = 0; i < rows; i++)
+        for(int j = 0; j < columns; j++)
+            array[i,j] = new Random().Next (minValue, maxValue + 1);
+
+    return array;
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i ++)
+    {
+        for(int j = 0; j < array.GetLength(1); j++)
+            Console.Write(array[i,j] + " ");
+        
+        Console.WriteLine();
+    }
+    Console.WriteLine(); 
+}
+*/
+/*Console.Write("In put a quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+ Console.Write("In put a quantity of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+Console.Write("In put a quantity of value: ");
+int minValue = Convert.ToInt32(Console.ReadLine());
+Console.Write("In put a quantity of value: ");
+int maxValue = Convert.ToInt32(Console.ReadLine());
+
+int[,] myArray = CreateRandom2dArray(rows, columns, minValue, maxValue);
+Show2dArray(myArray);
+*/
+
+
+/*Homework.
+Task 47. Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+ m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9
+*/
+/*
+
+void CreateRandom2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(-99, 99) / 10.0;
+        }
+    }
+}
+
+void Show2dArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+Console.Write("In put a quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+ Console.Write("In put a quantity of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+
+double[,] numbers = new double[rows, columns];
+CreateRandom2dArray(numbers);
+Show2dArray(numbers);
+*/
+/*
+Task 50. Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и 
+возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+1 7 -> числа с такими индексами в массиве нет
+*/
+/*
+void CreateRandom2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+void Show2dArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+ 
+Console.Write("In put a quantity of rows: ");
+int rows = Convert.ToInt32(Console.ReadLine());
+ Console.Write("In put a quantity of columns: ");
+int columns = Convert.ToInt32(Console.ReadLine());
+int[,] numbers = new int[6, 8];
+CreateRandom2dArray(numbers);
+Show2dArray(numbers);
+
+if (rows < numbers.GetLength(0) && columns < numbers.GetLength(1)) Console.WriteLine(numbers[rows, columns]);
+else Console.WriteLine($"{rows}{columns} -> такого числа в массиве нет");
+*/
+
+// int rows = ReadInt("Введите номер строки: ");
+// int colums = ReadInt("Введите номер столбца: ");
+
+/*Task 51. Задайте двумерный массив из целых чисел. Найдите среднее арифметическое элементов в каждом столбце.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+int rows = 3;
+int colums = 4;
+int[,] numbers = new int[rows, colums];
+FillArray2D(numbers);
+PrintArray2D(numbers);
+
+double[] avgNumbers = new double[numbers.GetLength(1)];
+
+for (int i = 0; i < numbers.GetLength(1); i++)
+{
+    double result = 0.0;
+    for (int j = 0; j < numbers.GetLength(0); j++)
+    {
+        result += numbers[j, i];
+    }
+    avgNumbers[i] = result / numbers.GetLength(0);
+}
+PrintArray(avgNumbers);
+
+
+
+void FillArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = new Random().Next(1, 10);
+        }
+    }
+}
+
+
+void PrintArray2D(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + " ");
+        }
+        Console.WriteLine();
+    }
+    Console.WriteLine();
+}
+
+
+void PrintArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+/*
